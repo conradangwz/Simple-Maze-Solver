ade38a718911b677d24bb82bea28502f47aad3c7
cang688 <cang688@aucklanduni.ac.nz>
2025-09-07 18:36:11 +1200

    began creating the simple maze solver in java

--- README.md
+++
@@ -1,2 +0,0 @@
-# Simple-Maze-Solver
-A simple maze sorter using the improved Dijkstra's algorithm by the Tsinghua University Scientists that runs on O(m/log^{2/3} n) time
---
+++ src/solver.java
@@ -0,0 +1,16 @@
+public class solver {
+
+    int[][] maze = {
+            {1, 0, 1, 1, 1, 1},
+            {1, 0, 0, 0, 0, 1},
+            {1, 1, 1, 0, 1, 1},
+            {1, 0, 0, 0, 0, 1},
+            {1, 1, 1, 1, 0, 1},
+            {1, 1, 1, 1, 2, 1}
+    };
+    // KEY: 0 = path, 1 = wall, 2 = goal
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
