c4f374534e684d7f1b7cf796fadaef1a49d8b4cb
cang688 <cang688@aucklanduni.ac.nz>
2025-09-07 18:46:12 +1200

    created class called Position to store location and LinkedList to store the positions

--- src/solver.java
+++ src/solver.java
@@ -1,4 +1,6 @@
-public class solver {
+import java.util.LinkedList;
+
+public class Solver {
 
     int[][] maze = {
             {1, 0, 1, 1, 1, 1},
@@ -10,7 +12,11 @@ public class solver {
     };
     // KEY: 0 = path, 1 = wall, 2 = goal
 
+    LinkedList<Position> path = new LinkedList<Position>();
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Position pos = new Position(3,0);
+        path.add(pos);
+
     }
 }
